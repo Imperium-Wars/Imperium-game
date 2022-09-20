929a2662e448fa159bb3a4984c77c141d5763037
OxBonaparte <>
2022-09-20 11:30:01 +0200

    ref: add responsive territory component

---
+++ components/Territories/mobileTerritory.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable @next/next/no-img-element */
+import { Modal, useMediaQuery } from "@mui/material";
+import React, { FunctionComponent } from "react";
+import styles from "../../styles/Territory.module.css";
+import { TerritoryData } from "./territoriesData";
+import TerritoryContent from "./territoryContent";
+
+type MobileTerritoryProps = {
+  territory: TerritoryData;
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const MobileTerritory: FunctionComponent<MobileTerritoryProps> = ({
+  territory,
+  isOpen,
+  onClose,
+}) => {
+  const isScreenSmall = useMediaQuery("(max-width:1024px)");
+
+  return (
+    <Modal
+      disableAutoFocus
+      open={isOpen}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <div className={styles.territoryModal}>
+        <button className={styles.territoryModalClose} onClick={onClose}>
+          <svg viewBox="0 0 24 24">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          </svg>
+        </button>
+        <h1 className={styles.countryName}>{territory.name}</h1>
+        <div className={styles.mobileTerritoryContainer}>
+          <div className={styles.mobileTerritoryImages}>
+            <img
+              className={styles.mobileTerritoryImage}
+              src={
+                territory?.isAtWar
+                  ? `/territories/${territory.name.replace(" ", "")}War.webp`
+                  : `/territories/${territory.name.replace(" ", "")}Peace.webp`
+              }
+              width={204}
+              alt="territory image"
+            />
+            <img
+              className={styles.mobileTerritoryCadre}
+              src="/squareCadre.webp"
+              width={273}
+              alt="territory image"
+            />
+          </div>
+          <div className={styles.mobileTerritoryContent}>
+            <TerritoryContent title="Continent" text={territory.continent} />
+            <TerritoryContent title="Territory type" text={territory.type} />
+            <TerritoryContent
+              title="Territory apogee"
+              text={territory.apogee}
+            />
+            <TerritoryContent title="History" text={territory.history} />
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default MobileTerritory;
--- pages/explore.tsx
+++ pages/explore.tsx
@@ -2,22 +2,35 @@ import type { NextPage } from "next";
 import Map from "../components/mapFiver";
 import styles from "../styles/map.module.css";
 import Territory from "../components/Territories/territory";
+import MobileTerritory from "../components/Territories/mobileTerritory";
 import { useMediaQuery } from "@mui/material";
 import { useState } from "react";
 import { TerritoryKey } from "../components/Territories/territoriesData";
 import TerritoriesData from "../components/Territories/territoriesData";
 
 const Explore: NextPage = () => {
-  const matches = useMediaQuery("(max-width:1024px)");
+  const isScreenSmall = useMediaQuery("(max-width:1024px)");
   const [territory, setTerritory] = useState<TerritoryKey>("China");
+  const [territoryModal, setTerritoryModal] = useState(false);
 
   function onClick(e: any) {
     setTerritory(e.target.id);
+    if (isScreenSmall) {
+      setTerritoryModal(true);
+    }
   }
 
   return (
     <div className={styles.mapPage}>
-      {matches ? null : <Territory territory={TerritoriesData[territory]} />}
+      {isScreenSmall ? (
+        <MobileTerritory
+          onClose={() => setTerritoryModal(false)}
+          isOpen={territoryModal}
+          territory={TerritoriesData[territory]}
+        />
+      ) : (
+        <Territory territory={TerritoriesData[territory]} />
+      )}
       <div className={styles.mapContainer}>
         <Map onClick={onClick} />
       </div>
