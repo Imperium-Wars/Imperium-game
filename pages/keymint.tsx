f062392118ab37fb11423555e7db13f5493321b3
OxBonaparte <>
2022-11-21 14:13:04 +0100

    fix: set new testnet contract

--- pages/keymint.tsx
+++ pages/keymint.tsx
@@ -25,8 +25,8 @@ const Cards: NextPage = () => {
 
   const callData = {
     contractAddress: keyContract,
-    entrypoint: "whitelistMint",
-    calldata: [randomTokenId, signature?.[0], signature?.[1]],
+    entrypoint: "whitelist_mint",
+    calldata: [randomTokenId, 0, signature?.[0], signature?.[1]],
   };
   const { execute, data } = useStarknetExecute({
     calls: callData,
@@ -70,21 +70,23 @@ const Cards: NextPage = () => {
 
   return (
     <div className={styles.container}>
+      <div className={styles.blackFilter}></div>
+
       {errorMessage ? (
         <div className={styles.main}>
-          <h1 className={styles.homeTitle}>You&apos;re not whitelisted</h1>
+          <h1 className={styles.homeTitle}>You are not whitelisted</h1>
           <Button onClick={disconnectByClick}>Try another account</Button>
         </div>
       ) : (
         <div className={styles.main}>
-          <video loop autoPlay muted className={styles.video} width={400}>
+          <video loop autoPlay muted className={styles.video} width={350}>
             <source src="/keyImperiumWars.mp4" />
           </video>
           <div className="mt-5">
             <Button
               onClick={isConnected ? () => mint() : () => setHasWallet(true)}
             >
-              Mint Your key
+              Mint your key
             </Button>
           </div>
         </div>
